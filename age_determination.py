615cd1a3406d1b303dcdf83f21561913d6cf0b82
Sazuppy <sazuppy@gmail.com>
2023-12-01 19:08:06 +0300

    Update age_determination.py

--- age_determination.py
+++ age_determination.py
@@ -12,19 +12,34 @@ st.write(
     - Анализировать покупки и предлагать товары, которые могут заинтересовать покупателей этой возрастной группы; 
     - Постройте модель, которая по фотографии определит приблизительный возраст человека."""
 )
-st.write("### Загрузите фотографию в формате jpg:")
+st.sidebar.header('Немного оинформации:')
+st.sidebar.write('''После запуска вам будет необходимо загрузить пиксельную квадратную фотографию, 
+                 в которую целиком помещается вся человеческая голова. 
+                 Нейронная сеть лучше всего работает на изображениях, в которых люди смотрят прямо в камеру. Пример:
+                 ''')
+st.sidebar.image("https://github.com/tg-bomze/Face-Depixelizer/raw/master/example.jpg")
+st.sidebar.write('''Обрезать фото вы можете 
+                [ТУТ](https://www.iloveimg.com/crop-image)
+                 ''')
+                 
+st.write("### Загрузите фотографию в формате: jpg, jpeg, png:")
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("")
 
+try: 
+    if uploaded_file:
+        loaded_model = tf.keras.saving.load_model("age_determination.h5")
 
-loaded_model = tf.keras.saving.load_model("age_determination.h5")
+        img = image.load_img(uploaded_file, target_size=(224, 224))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0
 
-img = image.load_img(uploaded_file, target_size=(224, 224))
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-img_array /= 255.0
-
-prediction = loaded_model.predict(img_array)
-predicted_age = prediction[0][0]
-st.image(uploaded_file)
-st.write(predicted_age)
+    
+    
+        prediction = loaded_model.predict(img_array)
+        predicted_age = prediction[0][0]
+        st.image(uploaded_file)
+        st.write(predicted_age)
+except:
+    st.write('Загрузите файл необходимого расширения')
